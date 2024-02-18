ac03c9070ab90dd9a865450121c30496db94225f
zhuqinfei <1586859762@qq.com>
2024-02-18 17:29:46 +0800

    枚举的比特位的用法

--- HelloEnum/Program.cs
+++ HelloEnum/Program.cs
@@ -17,6 +17,12 @@ namespace HelloEnum
             Console.WriteLine((int)Level.Manager);
             Console.WriteLine((int)Level.Boss);
             Console.WriteLine((int)Level.BigBoss);
+
+            person.Skill = Skill.Drive | Skill.Cook | Skill.Program | Skill.Teach;
+            //打印出来的值是累加的15，这是枚举的比特位用法
+            Console.WriteLine(person.Skill);
+            //看一个人是否具备煮饭功能
+            Console.WriteLine((person.Skill & Skill.Cook)> Skill.Cook);
         }
         enum Level
         {
@@ -26,11 +32,19 @@ namespace HelloEnum
             Boss=200,
             BigBoss
         }
+        enum Skill
+        {
+            Drive = 1,
+            Cook = 2,
+            Program =4,
+            Teach = 8,
+        }
         class Person
         {
             public int ID { get; set; }
             public string Name { get; set; }
             public Level Level { get; set; }
+            public Skill Skill { get; set; }
         }
     }
 }
